16a1841b8463b70b56a262adb2120a95b0db0681
Cristian <cristian_acdc15@hotmail.com>
2024-08-22 11:59:57 -0300

    update JWT

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([Authenticate::class])->group(function () {
     // Este grupo de rotas é protegido pela middleware AuthController
     Route::prefix('auth')->group(function () {
-        Route::post('login', [AuthController::class, 'login'])->withoutMiddleware([AuthController::class]);
+        Route::post('login', [AuthController::class, 'login'])->withoutMiddleware([Authenticate::class]);
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::post('me', [AuthController::class, 'me']);
